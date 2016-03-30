8fc309991021ea44b72fa8d5ba74f2bdfdb74aa8
DumDumGeniuss <dumdumgenius@gmail.com>
2016-03-30 18:43:49 +0800

    Fix devise.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-# config valid only for current version of Capistrano
-lock '3.4.0'
 
 set :application, 'knowledge_base'
 set :repo_url, 'git@github.com:DumDumGeniuss/knowledge_base.git'
@@ -18,6 +16,15 @@ namespace :deploy do
   end
 
   after :publishing, :restart
+
+   after :restart, :clear_cache do
+     on roles(:web), in: :groups, limit: 3, wait: 10 do
+       # Here we can do anything such as:
+       # within release_path do
+       #   execute :rake, 'cache:clear'
+       # end
+     end
+   end
 end
 
 # Default branch is :master
